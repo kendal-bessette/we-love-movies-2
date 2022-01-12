975720f2433f78f8a1c685e8be751f54bf6e63c4
Kendal Bessette <87912955+kendal-bessette@users.noreply.github.com>
2022-01-11 20:17:08 -0500

    more edits for deployment

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -3,21 +3,21 @@ const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/")
-.get(controller.list)
-.all(methodNotAllowed);
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 router.route("/:movieId")
-.get(controller.read)
-.all(methodNotAllowed);
+    .get(controller.read)
+    .all(methodNotAllowed);
 
-router.route("/:movieId/reviews")
-.get(controller.getReviewsByMovieId)
-.all(methodNotAllowed)
+router
+  .route("/:movieId/reviews")
+  .get(controller.getReviewsByMovieId)
+  .all(methodNotAllowed);
 
-router.route("/:movieId/theaters")
-.get(controller.getTheatersByMovieId)
-.all(methodNotAllowed);
-
-
+router
+  .route("/:movieId/theaters")
+  .get(controller.getTheatersByMovieId)
+  .all(methodNotAllowed);
 
 module.exports = router;
