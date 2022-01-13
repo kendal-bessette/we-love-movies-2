72a806580f762bcb9c90ff7960c3f4b28481f534
Kendal Bessette <87912955+kendal-bessette@users.noreply.github.com>
2022-01-12 21:25:56 -0500

    Edits to Procfile

--- src/theaters/theaters.router.js
+++ src/theaters/theaters.router.js
@@ -1,4 +1,4 @@
-const router = require("express").Router(); 
+const router = require("express").Router({ mergeParams: true }); 
 const theaterController = require("./theaters.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
