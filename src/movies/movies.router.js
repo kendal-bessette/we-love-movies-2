276a2be5951f90cf17f377eaaad9543595cf51d0
Kendal Bessette <87912955+kendal-bessette@users.noreply.github.com>
2022-01-13 13:48:24 -0500

    rearranged routes

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,22 +2,22 @@ const router = require("express").Router();
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/")
-    .get(controller.list)
-    .all(methodNotAllowed);
+router
+  .route("/:movieId/reviews")
+  .get(controller.getReviewsByMovieId)
+  .all(methodNotAllowed);
+  
+  router
+  .route("/:movieId/theaters")
+  .get(controller.getTheatersByMovieId)
+  .all(methodNotAllowed);
 
 router.route("/:movieId")
     .get(controller.read)
     .all(methodNotAllowed);
 
-router
-  .route("/:movieId/reviews")
-  .get(controller.getReviewsByMovieId)
-  .all(methodNotAllowed);
-
-router
-  .route("/:movieId/theaters")
-  .get(controller.getTheatersByMovieId)
-  .all(methodNotAllowed);
+  router.route("/")
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 module.exports = router;
